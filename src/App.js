de96d8f29cdbc619ac3f76d26e38f6b3e380dbd7
Robert Horosewski <horoserp@yahoo.com>
2025-02-04 18:58:25 -0500

    Make Card component.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import "./App.css";
-import EmailInput from "./EmailInput";
+import Card from "./components/Card";
 
 function App() {
   return (
     <main>
-      <article>
-        <div>
-          <header>
-            <img
-              src="/coming-soon/assets/images/logo.svg"
-              alt="Base Apparel Logo"
-            ></img>
-          </header>
-          <h1>We're coming soon</h1>
-          <p>
-            Hello fellow shoppers! We're currently building our new fashion
-            store. Add your email below to stay up-to-date with announcements
-            and our launch deals.
-          </p>
-          <EmailInput />
-        </div>
-        <div>
-          <img
-            src="/coming-soon/assets/images/hero-desktop.jpg"
-            alt="Woman modeling merchandise"
-          ></img>
-        </div>
-      </article>
+      <Card />
     </main>
   );
 }
