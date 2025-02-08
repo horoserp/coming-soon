30cc86662eb298db7f776b10498281f7d46a4d35
Robert Horosewski <horoserp@yahoo.com>
2025-02-08 00:04:17 -0500

    Fix border radius on input by adding padding.

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -3,6 +3,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import {
   Button,
+  Flex,
   FormControl,
   FormErrorMessage,
   FormLabel,
@@ -32,26 +33,35 @@ const EmailInput = () => {
         <FormLabel htmlFor="email" className="visually-hidden">
           Email Address
         </FormLabel>
-        <Input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="Email Address"
+        <Flex
+          className="formContainer"
+          borderRadius={30}
           w={"100%"}
           h={"55px"}
-          py={"10px"}
-          rounded={"30px"}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          required
-          className="input"
-        />
+          pl={25}
+        >
+          <Input
+            className="input"
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Email Address"
+            py={10}
+            border={"none"}
+            flexGrow={2}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            required
+          />
+          <Button type="submit" w={"100px"} h={"100%"} rounded={"30px"}>
+            <img
+              src="/coming-soon/assets/images/icon-arrow.svg"
+              alt="Submit"
+            ></img>
+          </Button>
+        </Flex>
         <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
       </FormControl>
-
-      <Button type="submit" w={"100px"} h={"55px"} rounded={"30px"}>
-        <img src="/coming-soon/assets/images/icon-arrow.svg" alt="Submit"></img>
-      </Button>
     </form>
   );
 };
